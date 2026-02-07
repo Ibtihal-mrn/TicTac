4af92d16c65fb65e3475b0650b9b039c017adb23
Ajnain Chaimae <matricule@ecam.be>
2026-02-08 00:37:31 +0100

    EmergencyButton

---
+++ include/EmergencyButton.h
@@ -0,0 +1,9 @@
+#ifndef EMERGENCYBUTTON_H
+#define EMERGENCYBUTTON_H
+
+#include <Arduino.h>
+
+void emergencyButton_init();
+bool emergencyButton_isPressed();  // exactement ce nom
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,42 @@
 #include "encoders.h"
 #include "motors.h"
 #include "control.h"
+#include "emergencyButton.h"
 
 void setup() {
   Serial.begin(9600);
   motors_init();
   encoders_init();
+  emergencyButton_init();
 }
 
 void loop() {
-  long left, right;
-  encoders_read(&left, &right);
+    // Si le bouton est pressé → tout s’arrête
+    if (emergencyButton_isPressed()) {
+        motors_applySpeeds(0, 0);   // STOP immédiat
+        delay(40);
+        return; // on quitte le loop avant tout calcul ou print
+    }
 
-  long dL, dR;
-  encoders_computeDelta(left, right, &dL, &dR);
+    // Sinon, on continue normalement
+    long left, right;
+    encoders_read(&left, &right);
 
-  int speedL, speedR;
-  control_computeSpeeds(dL, dR, speedL, speedR);
+    long dL, dR;
+    encoders_computeDelta(left, right, &dL, &dR);
 
-  motors_applySpeeds(speedL, speedR);
+    int speedL, speedR;
+    control_computeSpeeds(dL, dR, speedL, speedR);
 
-  long error = dL - dR; 
-  Serial.print("dL:");
-  Serial.print(dL);
-  Serial.print(" dR:");
-  Serial.print(dR);
-  Serial.print(" err:");
-  Serial.println(error);
+    motors_applySpeeds(speedL, speedR);
 
-  delay(40);
+    long error = dL - dR; 
+    Serial.print("dL:");
+    Serial.print(dL);
+    Serial.print(" dR:");
+    Serial.print(dR);
+    Serial.print(" err:");
+    Serial.println(error);
+
+    delay(40);
 }
