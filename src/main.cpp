ba077dd19b343b1da377bd2fb3055c1215fdb111
Ibtihal-mrn <mourninibtihal@gmail.com>
2026-02-12 19:03:45 +0100

    Test dy gyro + diff séquences

--- src/main.cpp
+++ src/main.cpp
@@ -21,75 +21,104 @@ void loop()
     return; 
   }
 
+  // -----------------------------------
+  // --------- Carré sans gyro ---------
+  // -----------------------------------
+  // delay(2000);
+  // robot_rotate(120, 140);
+  // delay(2000);
   // robot_move_distance(1000, 140);
-
-  // long l, r ; 
-  // encoders_read(&l, &r);
-
-  // static long lastL = 0, lastR = 0;
-
-  // Serial.print("dTicksL="); Serial.print(l - lastL);
-  // Serial.print(" dTicksR="); Serial.println(r - lastR);
+  // delay(2000);
+  // robot_rotate(120, 140);
+  // delay(2000);
+  // robot_move_distance(1000, 140);
+  // delay(2000);
+  // robot_rotate(120, 140);
+  // delay(2000);
+  // robot_move_distance(1000, 140);
+  // delay(2000);
+  // robot_rotate(120, 140);
+  // robot_stop();
   
 
-  // lastL = ticksL;
-  // lastR = ticksR;
+  // -----------------------------------
+  // --------- Carré avec gyro ---------
+  // -----------------------------------
 
-  // Carré
+  // robot_move_distance(1255, 140);
   // delay(2000);
-  // robot_rotate(120, 140);
+  // robot_rotate_gyro(90, 150);
   // delay(2000);
-  // robot_move_distance(1000, 140);
-  // delay(2000);
-  // robot_rotate(120, 140);
-  // delay(2000);
-  // robot_move_distance(1000, 140);
-  // delay(2000);
-  // robot_rotate(120, 140);
-  // delay(2000);
-  // robot_move_distance(1000, 140);
-  // delay(2000);
-  // robot_rotate(120, 140);
-  
 
-  // Serial.print("ticksL="); Serial.print(ticksL);
-  // Serial.print(" ticksR="); Serial.println(ticksR);
+  // robot_move_distance(1255, 140);
+  // delay(2000);
+  // robot_rotate_gyro(90, 150);
+  // delay(2000);
 
-  // Serial.print(digitalRead(ENC_R_A));
-  // Serial.print(" ");
-  // Serial.println(digitalRead(ENC_R_B));
-  // delay(50);
+  // robot_move_distance(1255, 140);
+  // delay(2000);
+  // robot_rotate_gyro(90, 150);
+  // delay(2000);
 
+  // robot_move_distance(1255, 140);
+  // delay(2000);
+  // robot_rotate_gyro(90, 150);
+  // delay(2000);
+
+  // robot_stop();
+
+
+
+  // -----------------------------------
+  // --------- Séquence de test ---------
+  // -----------------------------------
+
+  // robot_move_distance(1255, 140);
   // delay(2000);
 
   // bras_deployer();
   // delay(2000);
 
-  // robot_rotate(235, 140);
-  // delay(2000);
-  
-
-  // robot_move_distance(1000, 140);
+  // robot_rotate_gyro(180, 160);
   // delay(2000);
 
-  // bras_retracter();
-
-  
-
-
-  // bras_deployer();
+  // robot_move_distance(1250, 140);
   // delay(2000);
-
-  // robot_rotate(110, 140);
-
-  // robot_move_distance(1000, 140);
 
   // bras_retracter();
   // delay(2000);
 
-  robot_rotate_gyro(180, 150);
+  // robot_stop();
 
-  robot_stop();
+  
+
+
+ 
+
+
 
   runSequence = false; 
 } 
+
+
+// Print 
+// Serial.print("ticksL="); Serial.print(ticksL);
+// Serial.print(" ticksR="); Serial.println(ticksR);
+
+// Serial.print(digitalRead(ENC_R_A));
+// Serial.print(" ");
+// Serial.println(digitalRead(ENC_R_B));
+// delay(50);
+
+
+// long l, r ; 
+// encoders_read(&l, &r);
+
+// static long lastL = 0, lastR = 0;
+
+// Serial.print("dTicksL="); Serial.print(l - lastL);
+// Serial.print(" dTicksR="); Serial.println(r - lastR);
+
+
+// lastL = ticksL;
+// lastR = ticksR;
