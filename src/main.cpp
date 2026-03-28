cb5093cbb11038a00f23022bfc4420a329b7257d
Felix0427d <felixdecrouychanel@hotmail.com>
2026-03-28 10:42:30 +0100

    Correction par Vass

--- src/main.cpp
+++ src/main.cpp
@@ -1,268 +1,103 @@
-// main.cpp
 #include <Arduino.h>
-#include <Wire.h>
-
-// Hardware
-#include "robot.h"
-#include "bras.h"
-#include "Relais.h"
-#include "encoders.h"
-#include "StartSwitch.h"
-#include "TeamSwitch.h"
-#include "motors.h"
-
-// Hub sensors Coprocessor
-#include "i2c_comm.h"
-#include "us.h"
-
-// Config & Debug prints
-#include "utils.h"
-#include "Debug.h"
 #include "config.h"
 #include "globals.h"
+#include "encoders.h"
 
-// External Objects
-extern Motors motors;
-StartSwitch startSwitch(GPIO_NUM_8);
-TeamSwitch teamSwitch((gpio_num_t)TEAM_SWITCH_PIN);
-
-// Hardware Interrupt (Ultrasonic sensors Hub Obstacle detected).
-volatile bool emergencyStop = false;  //extern in globals.h
-void IRAM_ATTR stopISR() { emergencyStop = true; }
-
-
-// TODO: remove/obfuscate this :
-// const int RELAY_PIN = 41;
-const bool RELAY_ACTIVE_LOW = true;
-bool lastSwitchState = HIGH;   // INPUT_PULLUP
-bool sequenceDone = false;     // Pour éviter répétition
-
-
-
-// ================== SETUP ==================
-void setup()
-{
-  debugInit(115200,
-            DBG_FSM |
-            DBG_I2C_HUB
-            // DBG_MOTORS |
-            // DBG_SENSORS |
-            // DEBUG_TEAM_SWITCH |
-            // DBG_SERVO |
-            // DBG_ENCODER |
-            // DBG_MAGNET
-            // DBG_COMMS |  
-            // DBG_ENCODER |
-            // DBG_LAUNCH_TGR       
-            // comment to deactivate its related prints
-  );
-
-  // pinMode(SWITCH_PIN, INPUT_PULLUP);
-  // lastSwitchState = digitalRead(SWITCH_PIN);
-  // relais_init(RELAY_PIN, RELAY_ACTIVE_LOW);
-
-  // Init electro-aimant
-  const bool RELAY_ACTIVE_LOW = true;  
-  bool lastSwitchState = HIGH;   // INPUT_PULLUP
-  bool sequenceDone = false;     // Pour éviter répétition
-  pinMode(SWITCH_PIN, INPUT_PULLUP);
-  lastSwitchState = digitalRead(SWITCH_PIN);
-  relais_init(RELAY_PIN, RELAY_ACTIVE_LOW);
-  // relais_on();
-
-  // I2C Setup.
-  Wire.begin(SDA_PIN, SCL_PIN, 100000);
-  // initUSConfig();    //TODO:
-  delay(200);
-
-  // Utils.h
-  // printEsp32Info();
-  // i2c_scanner();
-
-  // ======== HARDWARE INIT ==========
-
-  pinMode(STOP_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(STOP_PIN), stopISR, RISING);
-
-  // Init Hardware et Robot
-  ESP32PWM::allocateTimer(0); // SERVO timer (doit rester ici)
-  ESP32PWM::allocateTimer(1); // SERVO timer (doit rester ici)
-  bras_init();                // must run FIRST
-  robot_init();
-
-  Serial.println("Setup Done.");
-
-  // startSwitch.begin();
-  teamSwitch.begin();
-
-  Serial.println("Waiting for start switch...");
-  // startSwitch.waitForStart();
-  Serial.println("Starting sequence..."); 
+// ---------- Pins ----------
+// ---------- Motor Functions ----------
+void stopMotors() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, 0); // PWM 0 = stop
+  analogWrite(ENB, 0);
 }
 
-void loop()
-{
+void forward(int leftSpeed, int rightSpeed) {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
 
-  static bool runSequence = true;  
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
 
-
-  if (!runSequence) {
-    return; 
-  }
-
-
-  if (teamSwitch.readTeam() == TeamSwitchTeam::A)
-  {
-    Serial.println("Equipe A");
-    
-
-    // ? ROUTINE :
-    // bras_deployer();              // déploie le bras
-    // delay(1000);                   // arrêt du robot
-    // bras_retracter();             // rétracte le bras
-    // delay(1000);                   // arrêt du robot
-    
-    // bras_deployer();              // déploie le bras
-    // delay(1000);                   // arrêt du robot
-    // bras_retracter();             // rétracte le bras
-    // delay(1000);  
-
-    // driveDistancePID(900, 254);
-        rotateAnglePID(-180, 200);      // tourne à droite de 90°
-
-    // driveDistancePID(900, 200);
-    // delay(200);
-    // rotateAnglePID(90, 200);
-    // delay(200);
-    // driveDistancePID(500, 200);
-
-
-    // driveDistancePID(1000, 200);   // avance 15 cm
-    // delay(1000);                   // arrêt du robot
-
-    // bras_deployer();              // déploie le bras
-    // delay(1000);                   // arrêt du robot
-
-    // rotateAnglePID(180, 200);      // tourne à droite de 90°
-    // delay(1000);                   // arrêt du robot
-
-    // driveDistancePID(1000, 200);    // avance 100 cm
-    // delay(1000);                   // arrêt du robot
-
-    // driveDistancePID(-500, 200);    // recule 50 cm
-    // delay(1000);                   // arrêt du robot
-
-    // rotateAnglePID(-90, 200);      // tourne à gauche de 90°
-    // delay(1000);                   // arrêt du robot 
-
-    // driveDistancePID(500, 254);     // avance 50 cm
-    delay(1000);                   // arrêt du robot
-    
-     bras_deployer();              // déploie le bras
-    delay(1000);                   // arrêt du robot
-    bras_retracter();             // rétracte le bras
-    delay(1000);  
-  }
-
-  else
-  {
-    // Serial.println("Equipe B");
-    // driveDistancePID(1000, 254);
-    // delay(1000);
-    // // bras_deployer();
-    // // delay(2000);
-    // // robot_rotate_gyro(180, 200);
-    // // delay(1000);
-    // // driveDistancePID(500, 254);
-    // // delay(1000);
-    // // bras_retracter();
-    // driveDistancePID(500, 200);   // avance
-    rotateAnglePID(180, 200);      // tourne à droite de 90°
-    delay(500);                    // arrêt
-
-    relais_on();                   // active le relais
-    delay(500);                   // attendre 5 secondes
-
-    // driveDistancePID(500, 200);   // avance
-    // delay(500);                   // ← ce delay ne sert pas d'arrêt, c'est trop long
-
-    relais_off();                  // désactive le relais
-    delay(500);
-
-    // driveDistancePID(500, 200);   // avance
-    }
-
-
-  runSequence = false;
+  analogWrite(ENA, constrain(leftSpeed, 0, 255));
+  analogWrite(ENB, constrain(rightSpeed, 0, 255));
 }
 
-  //Servo Test
-  // bras_deployer();
-  // delay(2000);
-  // bras_retracter();
-  // delay(2000);
+void backward(int leftSpeed, int rightSpeed) {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
 
-  //driveDistancePID(-1000, 254);
-  //driveDistancePID(500, 254);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
 
-  // imAlive();
-  // printEncodersVal();
-  // printUltrasonicVal();
-  // imAlive();
-  // printEncodersVal();
-  // printUltrasonicVal();
+  analogWrite(ENA, constrain(leftSpeed, 0, 255));
+  analogWrite(ENB, constrain(rightSpeed, 0, 255));
+}
 
-  // Servo Test
-  //  bras_deployer();
-  //  delay(2000);
-  //  bras_retracter();
-  //  delay(2000);
-  //  bras_deployer();
-  //  delay(2000);
-  //  bras_retracter();
-  //  delay(2000);
+void rotateRight(int speed) {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
 
-  // driveDistancePID(500, 254);
-  // delay(1000);
-  // driveDistancePID(-500, 254);
-  // delay(1000);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
 
-  // rotateAnglePID(90, 200);
+  analogWrite(ENA, speed);
+  analogWrite(ENB, speed);
+}
 
+void rotateLeft(int speed) {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
 
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+
+  analogWrite(ENA, speed);
+  analogWrite(ENB, speed);
+}
 
 
-// void loop() {
-//     printUltrasonicVal();  // Décommente
-//     Serial.print("Obstacle? ");
-//     Serial.println(ultrasonic_isObstacle() ? "OUI" : "NON");
-//     delay(500);
-// }
+// ---------- Setup ----------
+void setup() {
+  Serial.begin(115200);
 
-// void loop() {
-//     Serial.println("=== DEBUG SAFETY ===");
-//     Serial.print("emergencyButton: "); Serial.println(emergencyButton_isPressed() ? "OUI" : "NON");
-//     Serial.print("ultrasonic obs: "); Serial.println(ultrasonic_isObstacle() ? "OUI" : "NON");
-//     Serial.print("safety_update: "); Serial.println(safety_update() ? "STOP" : "OK");
-//     delay(200);
-// }
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
 
-// void loop() {
-//     Serial.println("=== DEBUG DRIVE ===");
-//     Serial.print("safety_update: "); Serial.println(safety_update() ? "STOP" : "OK");
-//     if (safety_update()) Serial.println("*** WOULD STOP ***");
-//     delay(200);
-// }
+  pinMode(ENB, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 
-// void loop() {
-//     Serial.println("START MOTORS");
-//     motors.forward(150, 150);  // ← Démarre
-//     delay(3000);
+  encoders_init();
 
-//     Serial.println("STOP TEST");
-//     motors.stopMotors();       // ← Test
-//     delay(3000);
+  // Stop motors initially
+  stopMotors();
+}
 
-//     Serial.println("---");
-// }
-  //runSequence = false;
+
+// ---------- Loop Example ----------
+void loop() {
+  Serial.println("Forward");
+  forward(200, 200);
+  printEncodersVal();
+  delay(2000);
+
+  Serial.println("Stop");
+  stopMotors();
+  printEncodersVal();
+  delay(1000);
+
+  Serial.println("Backward");
+  backward(200, 200);
+  printEncodersVal();
+
+  delay(2000);
+
+  Serial.println("Stop");
+  stopMotors();
+  printEncodersVal();
+  delay(10000);
+}
